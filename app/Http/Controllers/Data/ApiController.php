de28ea7ad897f8a782861e6afae5a62a8738abcc
blacktomang <blacktomang@gmail.com>
2021-03-02 22:12:40 +0700

    create apiConfig

--- app/Http/Controllers/Data/ApiController.php
+++ app/Http/Controllers/Data/ApiController.php
@@ -10,10 +10,12 @@ class ApiController extends Controller
 {
     function fetchAPI(Request $request)
     {
-        $key = '';
-        if ($request->has("params")) {
-            $key = $request->query("params");
-        }else {
+        $key = $request->query("params");
+        $value = '';
+        $data = config('api.key');
+        if (isset($data[$key])) {
+            $value = $data[$key];
+        } else {
             return view('welcome');
         }
         date_default_timezone_set('Asia/Jakarta');
@@ -21,7 +23,7 @@ class ApiController extends Controller
             'client_key' => 'qJRCK04wWxcoINai',
             'date' => date("j"),
             'night' => 1,
-            'key_prop' => $key,
+            'key_prop' => $value,
         ]);
         $homstayName = json_decode($response->body())->prop->namaHomestay;
         return redirect('/user')->with('homeStayName', $homstayName);
